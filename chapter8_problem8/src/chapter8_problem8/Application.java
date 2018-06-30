d01189afb845fa58bfd67a4544864bb075fe3e74
Gino <cafeTechne@gmail.com>
2018-06-30 02:57:00 -0400

    updated the problem to meet spec

--- chapter8_problem8/src/chapter8_problem8/Application.java
+++ chapter8_problem8/src/chapter8_problem8/Application.java
@@ -48,38 +48,35 @@ public class Application {
 		//tests passed!
 		
 		
-		//bring it all together by testing a parking ticket
+		/*****************************************************************************************/
 		//instantiate a parking ticket object
+		//moved this call to the PoliceOfficer object !!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		//ParkingTicket ticket = new ParkingTicket();
+		//ticket.setOfficerIssuingTicket(cop);
 		
-		ParkingTicket ticket = new ParkingTicket();
-		ticket.setOfficerIssuingTicket(cop);
-		
-
-		System.out.println(ticket.getOfficerName());
-		System.out.println(ticket.getBadgeNumber());
+		//System.out.println(ticket.getOfficerName());
+		//System.out.println(ticket.getBadgeNumber());
 		//calling objects data fields are correctly set in the ticket object--tests passed!
 		
 		//set remaining data fields of the ticket object
 		//pass a reference to the car object to set data fields in parking ticket
-		ticket.setAssociatedCar(automobile);
+		//ticket.setAssociatedCar(automobile);
 		
-		ticket.ticketInfo();
+		//ticket.ticketInfo();
 		//all fields we changed are working, we just need to set the fine amount now
 		//and make sure that our math works out!
 		
-		ticket.setFineAmount(automobile, meter);
+		//ticket.setFineAmount(automobile, meter);
 		//when the amount of time over is 121, we get 45.00 as the fine amount--which is expected
 		//when the amount of time is 120, we get 35.00 which is what is expected!--ALL TESTS PASSED!
 		//::HIGH FIVE::
 		
-		ticket.ticketInfo();
-		
-		
-		
-		
-		
-		
-		
+		//ticket.ticketInfo();
+
+		/*******************************************************************************************/
+		//moved all of the above logic into the PoliceOfficer class --here we see encapsulation and abstraction at work
+		//TESTS PASSED!! 
+		cop.examineCarForParkingViolation(automobile, meter);
 		
 		
 		
--- chapter8_problem8/src/chapter8_problem8/PoliceOfficer.java
+++ chapter8_problem8/src/chapter8_problem8/PoliceOfficer.java
@@ -26,6 +26,30 @@ public class PoliceOfficer {
 	
 	
 	
+	/** 
+	 *  to examine a parkedCar object and a Parking meter object, and determine whether the car's time has expired 
+	 * @param car
+	 * @param meter
+	 * @return true means that there is a parking violation
+	 */
+	
+	public void examineCarForParkingViolation(ParkedCar car, ParkingMeter meter)
+	{
+		if(car.getNumberOfMinutesParked() > meter.getParkingTimePurchased()) issueTicket(car, meter);
+		
+	}
+	
+	
+	public void issueTicket(ParkedCar car, ParkingMeter meter) {
+		
+		ParkingTicket ticket = new ParkingTicket();
+		ticket.setOfficerIssuingTicket(this);
+		ticket.setAssociatedCar(car);
+		ticket.setFineAmount(car, meter);
+		ticket.ticketInfo();
+	}
+	
+	//to issue a parking ticket (generate a parking ticket object) if the car's time has expired
 	
 	
 }
